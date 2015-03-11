9fe3fbd7f717c30d0775501b339ea8f0e528aa5b
Maria Aranibar <saranibarp@outlook.com>
2015-03-11 22:27:29 +0000

    Added validations

--- db/migrate/20150311221302_create_users.rb
+++ db/migrate/20150311221302_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :name, default: ''
+      t.string :email, null: false
+      t.string :password_digest, null: false
       t.timestamps null: false
     end
   end
